01c0d9b045097662968a5b79e6088dd7ea2c2d72
kmx227 <rlaalstjq227@gmail.com>
2021-06-12 23:41:28 +0900

    portfolio

---
+++ Portfolio/Maze_Scripts/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] Items;
+    public int value;
+    public GameObject realItem;
+    public GameObject handItem;
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+   
+    }
+}
